5e6436f4e049a112ee898d1ae5a5a07c0b44c9c9
Mandipgit <mandippokharel456@gmail.com>
2025-02-26 15:25:19 +0545

    Quick Update

--- lib/Dashhboard/dashboard.dart
+++ lib/Dashhboard/dashboard.dart
@@ -16,17 +16,19 @@ class Dashboardpage extends StatefulWidget {
 class _DashboardpageState extends State<Dashboardpage> {
   Color blueColor = Color(0xFF167AFA);
   Color greyColor = Color.fromARGB(255, 228, 224, 224);
-  bool iconChecked=false;
+  bool iconChecked = false;
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        backgroundColor: Colors.white,
+        backgroundColor: greyColor,
         appBar: AppBar(
-          title: Text("Teacher",
-          style: TextStyle(
-            color: Colors.white,
-          ),),
+          title: Text(
+            "Teacher",
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
           backgroundColor: blueColor,
           centerTitle: true,
           iconTheme: IconThemeData(color: Colors.white),
@@ -42,7 +44,7 @@ class _DashboardpageState extends State<Dashboardpage> {
           ],
         ),
         drawer: Drawer(
-          width: MediaQuery.of(context).size.width * 0.8,
+          // width: MediaQuery.of(context).size.width * 0.8,
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.zero,
           ),
@@ -59,7 +61,6 @@ class _DashboardpageState extends State<Dashboardpage> {
               // const SizedBox(height:10),
               Container(
                 height: MediaQuery.of(context).size.height * 0.64,
-                
                 width: MediaQuery.of(context).size.width * 0.8,
                 decoration: BoxDecoration(
                   color: greyColor,
@@ -69,101 +70,142 @@ class _DashboardpageState extends State<Dashboardpage> {
             ],
           ),
         ),
-        body: Padding(
-              padding: const EdgeInsets.only(top: 20,left: 20,right:20),
-              child: Container(
-                height: 150,
-                width: double.maxFinite,
+        body: Column(children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 20, left: 15, right: 15),
+            child: Container(
+              height: 150,
+              width: double.maxFinite,
               decoration: BoxDecoration(
-                  color: blueColor,
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text("Hello Mandip",
-                          style:TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.w600,
-                            fontSize: 20,
-                          )
-                          ),
-                          const SizedBox(height: 5),
-                         Row(
-                           children: [
-                             !iconChecked?Container(
-                              height: 40,
-                              width: 213,
-                              decoration: BoxDecoration(
-                                color: blueColor,
-                              ),
-                              child: Row(
-                             
-                                children: [
-                                  Text("Mark Your Attendance",
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.w400,
-                                    fontSize: 16,
+                color: blueColor,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Hello Mandip",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w600,
+                              fontSize: 20,
+                            )),
+                        const SizedBox(height: 5),
+                        Row(
+                          children: [
+                            !iconChecked
+                                ? Container(
+                                    height: 40,
+                                    width: 213,
+                                    decoration: BoxDecoration(
+                                      color: blueColor,
+                                    ),
+                                    child: Row(
+                                      children: [
+                                        Text(
+                                          "Mark Your Attendance",
+                                          style: TextStyle(
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.w400,
+                                            fontSize: 16,
+                                          ),
+                                        ),
+                                        const SizedBox(width: 1),
+                                        IconButton(
+                                          onPressed: () {
+                                            setState(() {
+                                              iconChecked = !iconChecked;
+                                            });
+                                          },
+                                          icon: Icon(Icons.add_box_outlined),
+                                          color: Colors.white,
+                                        ),
+                                      ],
+                                    ),
+                                  )
+                                : Container(
+                                    height: 40,
+                                    width: 213,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10),
+                                      color: greyColor,
+                                    ),
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(10),
+                                      child: Text(
+                                        "Attendance Done",
+                                        style: TextStyle(
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.w600,
+                                          color: blueColor,
+                                        ),
+                                      ),
+                                    ),
                                   ),
-                                  ),
-                                  const SizedBox(width:1),
-                                  IconButton(onPressed: (){
-                                    setState(() {
-                                      iconChecked=!iconChecked;
-                                    });
-                                  }, 
-                                  icon:Icon(Icons.add_box_outlined),
-                                  color: Colors.white,
-                                  ),
-                                 ],
-                              ),
-                             ):Container(
-                               height: 40,
-                              width: 213,
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(10),
-                                color: greyColor,
-                              ),
-                             
-                                child: Padding(
-                                  padding: const EdgeInsets.all(10),
-                                  child: Text("Attendance Done",
-                                  style: TextStyle(
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.w600,
-                                    color: blueColor,
-                                  ),
-                                  ),
-                                ),
-                              ),
-                             ],
-                         )
-                        ],
-                      ),
-                      const SizedBox(width: 30),
-                          Padding(
-                            padding: const EdgeInsets.only(top: 15),
-                            child: CircleAvatar(
-                              radius:45 ,
-                              backgroundColor: Colors.white,
-                            ),
-                          ),
                           ],
+                        )
+                      ],
                     ),
-                ),
+                    const SizedBox(width: 30),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 15),
+                      child: CircleAvatar(
+                        radius: 45,
+                        backgroundColor: Colors.white,
+                      ),
+                    ),
+                  ],
                 ),
               ),
-              
-              )
-        
-      );
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 20),
+            child: Container(
+              height: MediaQuery.of(context).size.height * 0.645,
+              width: double.maxFinite,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(50),
+                    topRight: Radius.circular(50)),
+                color: Colors.white,
+              ),
+              // child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 8.0, left: 20),
+                      child: Container(
+                        height: 45,
+                        width: 200,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          // borderRadius: BorderRadius.only(topLeft:Radius.circular(50),topRight: Radius.circular(50))
+                        ),
+                        child: Text(
+                          "Explore Categories",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.w600,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                    ),
+                    
+                  ],
+                ),
+              ),
+            ),
+          // ),
+        ]),
+      ),
+    );
   }
 }
