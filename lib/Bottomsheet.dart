61c7d098f0a245ab1ff563deb3e4a3b367840c58
Mandipgit <mandippokharel456@gmail.com>
2025-03-05 17:54:15 +0545

    Quick Update

--- lib/Bottomsheet.dart
+++ lib/Bottomsheet.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:teachers/Dashhboard/dashboard.dart';
-import 'package:teachers/Pages%20from%20Dashboard/Assignment/B.tech/B.tech1st.dart';
 import 'package:teachers/Pages%20from%20Dashboard/Assignment/assignment.dart';
 import 'package:teachers/Pages%20from%20Dashboard/Assignment/test.dart';
 
-List<String>faculties=['Bsc.CSIT','BIT','B.Tech','BND','Physics','Geology'];
-List <String>Semester=['1stSemester','2ndSemester','3rdSemester','4thSemester','5thSemester','6thSemester','7thSemester','8thSemester'];
+final List<String>faculties=['BSc.CSIT','BIT','B.Tech','BND','Physics','Geology'];
+final List <String>Semester=['1stSemester','2ndSemester','3rdSemester','4thSemester','5thSemester','6thSemester','7thSemester','8thSemester'];
+final List <String>Year=['1st Year','2nd Year','3rd Year','4th Year'];
 showBottomSheetFaculty(BuildContext context){
   showModalBottomSheet(context: context, builder: (context){
     return Container(
@@ -25,6 +25,14 @@ showBottomSheetFaculty(BuildContext context){
                  color: Colors.white,
                  fontSize: 20,
                  fontWeight: FontWeight.w600,)),
+                 onTap: (){
+                 if(faculties[index]=='BSc.CSIT'||faculties[index]=='BIT'){
+                  showBottomSheetSemester(context,faculties[index]);
+                 }
+                 else {
+                  showBottomSheetYear(context,faculties[index]);
+                 }
+                    },
            );
          }),
       )  
@@ -32,201 +40,63 @@ showBottomSheetFaculty(BuildContext context){
     
   });
 }
-void showBottomSheetSemester(BuildContext context){
+showBottomSheetSemester(BuildContext context,String faculty){
   showModalBottomSheet(context: context, builder: (context){
     return Container(
-      height: MediaQuery.of(context).size.height*0.52,
+      height: MediaQuery.of(context).size.height*0.43,
       decoration: BoxDecoration(
-        color:secBlueColor,
-        borderRadius: BorderRadius.only(topLeft: Radius.circular(20),topRight: Radius.circular(20)),
+        color: secBlueColor,
+        borderRadius: BorderRadius.only( topLeft: Radius.circular(20),  topRight: Radius.circular(20)),
       ),
       child: Padding(
         padding: const EdgeInsets.all(10),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ListView.builder(
-              itemCount: Semester.length,
-              itemBuilder: (context,index){
-                ListTile(
-                
-                 );
-              })
-          ],
-        ),
-      ),
+        child: ListView.builder(
+         itemCount:Semester.length,
+         itemBuilder: (context, index){
+           return ListTile(
+             title: Text(Semester[index],
+              style: TextStyle(
+                 color: Colors.white,
+                 fontSize: 20,
+                 fontWeight: FontWeight.w600,)),
+                 onTap: (){
+                  Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context)=>assignmentPage(faculty: faculty, Semester: Semester[index])));
+                 },
+           );
+         }),
+      )  
     );
-  });
+    });
 }
-// void showBottomSheetYear(BuildContext context){
-//   showModalBottomSheet(context: context, builder: (context){
-//     return Container(
-//        height: MediaQuery.of(context).size.height*0.27,
-//       decoration: BoxDecoration(
-//         color:secBlueColor,
-//         borderRadius: BorderRadius.only(topLeft: Radius.circular(20),topRight: Radius.circular(20)),
-//       ),
-//       child: Padding(
-//         padding: const EdgeInsets.all(10),
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: [
-//             ListTile(
-//               title: Text('1st Year',
-//                style: TextStyle(
-//                   color: Colors.white,
-//                   fontSize: 20,
-//                   fontWeight: FontWeight.w600,)),
-//                                 onTap: (){
-                    
-// take_assignment?(
-//                 assignment=1,
-//                 Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context)=>assignment_Page()))
-//               ):(
-//                 test=1,
-//                 Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context)=>test_Page()))
-//               );
-//                   },
-//             ),
-            
-//             ListTile(
-//               title: Text('2nd Year',
-//                style: TextStyle(
-//                   color: Colors.white,
-//                   fontSize: 20,
-//                   fontWeight: FontWeight.w600,)),
-//                                 onTap: (){
-                    
-// take_assignment?(
-//                 assignment=2,
-//                 Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context)=>assignment_Page()))
-//               ):(
-//                 test=2,
-//                 Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context)=>test_Page()))
-//               );
-//                   },
-//             ),
-            
-//             ListTile(
-//               title: Text('3rd Year',
-//                style: TextStyle(
-//                   color: Colors.white,
-//                   fontSize: 20,
-//                   fontWeight: FontWeight.w600,)),
-//                                 onTap: (){
-                    
-// take_assignment?(
-//                 assignment=3,
-//                 Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context)=>assignment_Page()))
-//               ):(
-//                 test=3,
-//                 Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context)=>test_Page()))
-//               );
-//                   },
-//             ),
-            
-//             ListTile(
-//               title: Text('4th Year',
-//                style: TextStyle(
-//                   color: Colors.white,
-//                   fontSize: 20,
-//                   fontWeight: FontWeight.w600,)),
-//                                 onTap: (){
-                    
-// take_assignment?(
-//                 assignment=4,
-//                 Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context)=>assignment_Page()))
-//               ):(
-//                 test=4,
-//                 Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context)=>test_Page()))
-//               );
-//                   },
-//             ),
-//         ]),
-//       ),
-//     );
-  
-//   });
-// }
-// void showBottomSheetStudent(BuildContext context){
-//   showModalBottomSheet(context: context, builder: (context){
-//     return Container(
-//       height: MediaQuery.of(context).size.height*0.25,
-//       decoration: BoxDecoration(
-//         color: secBlueColor,
-//         borderRadius: BorderRadius.only( topLeft: Radius.circular(20),  topRight: Radius.circular(20)),
-//       ),
-//        child: Row(
-//             mainAxisAlignment: MainAxisAlignment.center,
-//             crossAxisAlignment: CrossAxisAlignment.center,
-//             children: [
-//               GestureDetector(
-//                 child: Container(
-//                   height: 130,
-//                   width: 130,
-//                   decoration: BoxDecoration(
-//                     color:greyColor,
-//                     borderRadius: BorderRadius.only( topLeft: Radius.circular(20),  topRight: Radius.circular(20)),
-//                   ),
-//                   child: Column(
-//                     mainAxisAlignment: MainAxisAlignment.center,
-//                     crossAxisAlignment: CrossAxisAlignment.center,
-//                     children: [
-//                       Padding(
-//                         padding: const EdgeInsets.only(top: .0),
-//                         child: Image.asset('Assets/attendance.png',
-//                         height:80,
-//                         ),
-//                       ),
-//                       Text('Attendance',
-//                       style: TextStyle(
-//                         fontSize: 18,
-//                         fontWeight: FontWeight.w500,
-//                       ),)
-//                     ],
-//                   ),
-//                 ),
-//                 onTap: (){
-                  
-//                 },
-//               ),
-//               const SizedBox(width: 40),
-//               GestureDetector(
-//                 child: Container(
-//                   height: 130,
-//                   width: 130,
-//                   decoration: BoxDecoration(
-//                     color:greyColor,
-//                     borderRadius: BorderRadius.circular(20),
-//                   ),
-//                   child: Column(
-//                     mainAxisAlignment: MainAxisAlignment.center,
-//                     crossAxisAlignment: CrossAxisAlignment.center,
-//                     children: [
-//                       Padding(
-//                         padding: const EdgeInsets.only(top: 8.0),
-//                         child: Image.asset('Assets/student_details.png',
-//                         height: 80,
-//                         ),
-//                       ),
-//                       Text('Student Details',
-//                       style: TextStyle(
-//                         fontSize: 18,
-//                         fontWeight: FontWeight.w500,
-//                       ),
-//                       ),
-//                     ],
-//                   ),
-//                 ),
-//                 onTap: () {
-//                   showBottomSheetFaculty(context);
-//                 },
-//               ),
-//             ],
-//           ),
-//     );
-//   });
-// }
+showBottomSheetYear(BuildContext context,String faculty){
+  showModalBottomSheet(context: context, builder: (context){
+    return Container(
+      height: MediaQuery.of(context).size.height*0.3,
+      decoration: BoxDecoration(
+        color: secBlueColor,
+        borderRadius: BorderRadius.only( topLeft: Radius.circular(20),  topRight: Radius.circular(20)),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: ListView.builder(
+         itemCount:Year.length,
+         itemBuilder: (context, index){
+           return ListTile(
+             title: Text(Year[index],
+              style: TextStyle(
+                 color: Colors.white,
+                 fontSize: 20,
+                 fontWeight: FontWeight.w600,)),
+                 onTap: (){
+                  Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context)=>assignmentPage(faculty: faculty, Semester: Year[index])));
+                 },
+                 );
+         }),
+      )  
+    );
+    });
+}
+
 void showBottomSheetAssignment(BuildContext context) {
   showModalBottomSheet(
       context: context,
@@ -309,12 +179,8 @@ void showBottomSheetAssignment(BuildContext context) {
         );
       });
 }
-// void showFacultySemester(BuildContext context, Faculty,int Semester){
-// List <int> navigation=[Faculty,Semester];
-// switch (navigation){
-//   case(1,1): Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context)=>Btech1st()))
-// }
-// }
+
+
 
 
  
---
+++ lib/Pages from Dashboard/Assignment/assignment.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:teachers/Dashhboard/dashboard.dart';
+
+class assignmentPage extends StatefulWidget {
+  final String faculty;
+  final String Semester;
+  const assignmentPage({required this.faculty,required this.Semester});
+  
+
+  @override
+  State<assignmentPage> createState() => _assignmentPageState();
+}
+
+class _assignmentPageState extends State<assignmentPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+         backgroundColor: blueColor,
+          title: Text(widget.faculty,
+          style: TextStyle(
+            color: Colors.white,
+          ),),
+          centerTitle: true,
+          leading: IconButton(onPressed: (){
+            setState(() {
+              Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context)=>Dashboardpage()));
+            });
+          },
+          color: Colors.white,
+          icon: Icon(Icons.arrow_back)),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              height: 45,
+              width: double.maxFinite,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: secBlueColor,
+              ),
+              child: Center(
+                child: Text(widget.Semester,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w600,
+                ),),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
